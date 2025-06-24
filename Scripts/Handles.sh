f61349b87308facd6b697402e8d9c885f5255b1d
chenxin <chenxin_verified@163.com>
2025-06-24 18:07:44 +0800

    Fix: OpenClash 集成内核问题

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -77,10 +77,18 @@ if [ -f "$RUST_FILE" ]; then
 fi
 
 #集成OpenClash内核
+CLASH_CORE_DIR=$PKG_PATH/luci-app-openclash/root/etc/openclash/core
+echo " "
+echo Downloading clash-linux-"$CPU_MODEL".tar.gz...
 curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-"$CPU_MODEL".tar.gz -o /tmp/clash.tar.gz
 tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
 chmod +x /tmp/clash >/dev/null 2>&1
-mkdir -p ./luci-app-openclash/files/etc/openclash/core
-mv /tmp/clash ./luci-app-openclash/files/etc/openclash/core/clash_meta >/dev/null 2>&1
+mkdir -p $CLASH_CORE_DIR
+mv /tmp/clash $CLASH_CORE_DIR/clash_meta >/dev/null 2>&1
 rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
-cd $PKG_PATH && echo "OpenClash core has been installed!"
+if [ -x "$CLASH_CORE_DIR/clash_meta" ]; then
+	echo "OpenClash core has been installed!"
+else
+	echo "OpenClash core hasn't been installed correctly!"
+fi
+cd $PKG_PATH
